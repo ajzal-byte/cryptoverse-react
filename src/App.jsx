bd136aed971919aa3a7c004982122cd799ba4a48
ajzal-byte <ajzalaju10@gmail.com>
2024-03-18 00:06:19 +0530

    Layouts added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,58 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import {  Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Cryptocurrencies,
+  CryptoDetails,
+  Exchanges,
+  HomePage,
+  Navbar,
+  News,
+} from "./components";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>hello</div>
-    </>
-  )
-}
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<HomePage />}></Route>
+              <Route exact path="/exchanges" element={<Exchanges />}></Route>
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              ></Route>
+              <Route
+                exact
+                path="/crypto/:coinId"
+                element={<CryptoDetails />}
+              ></Route>
+              <Route exact path="/news" element={<News />}></Route>
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Cryptocurrencies from "./Cryptocurrencies";
+import CryptoDetails from "./CryptoDetails";
+import Exchanges from "./Exchanges";
+import HomePage from "./HomePage";
+import Navbar from "./Navbar";
+import News from "./News";
+
+export { Cryptocurrencies, CryptoDetails, Exchanges, HomePage, Navbar, News };
