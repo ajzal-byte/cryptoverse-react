fc6c493f77f9c65a27d411e0da583f16082d1731
ajzal-byte <ajzalaju10@gmail.com>
2024-03-18 20:41:56 +0530

    news fetched

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,15 @@
-const News = () => {
+import { Select, Typography, Row, Col, Avatar, Card } from "antd"
+import moment from "moment"
+
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi"
+
+const {Text, Title} = Typography;
+const {Option} = Select
+
+const News = ({simplified}) => {
+  const count = simplified ? 10 : 100;
+  const {data: cryptoNews} = useGetCryptoNewsQuery(count)
+  console.log(cryptoNews);
   return (
     <div>News</div>
   )
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,31 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "X-RapidAPI-Key": "ad2a1b4b72mshafbcdf3ab87db2dp1372c3jsn26920ffa9aa2",
+  "X-RapidAPI-Host": "news67.p.rapidapi.com",
+};
+
+const baseUrl = "https://news67.p.rapidapi.com/v2";
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl,
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", cryptoNewsHeaders["X-RapidAPI-Key"]);
+      headers.set("X-RapidAPI-Host", cryptoNewsHeaders["X-RapidAPI-Host"]);
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: (count) => ({
+        url: "/crypto",
+        params: { batchSize: String(count) },
+      }),
+    }),
+  }),
+});
+
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
