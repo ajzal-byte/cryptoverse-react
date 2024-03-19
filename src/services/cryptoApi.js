4c60e64b3796f0103cadb19eb05fcdd50f94fa42
ajzal-byte <ajzalaju10@gmail.com>
2024-03-19 12:38:01 +0530

    CryptoDetails page data displayed

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -21,7 +21,10 @@ export const cryptoApi = createApi({
     getCryptos: builder.query({
       query: (count) => `/coins?limit=${count}`,
     }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => `/coin/${coinId}`,
+    }),
   }),
 });
 
-export const { useGetCryptosQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery } = cryptoApi;
